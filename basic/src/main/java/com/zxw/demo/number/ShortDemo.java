f5b6954ed5e3b463204147cd30672e2220fb4f51
Ryan <9726998998@qq.com>
2025-06-07 16:07:08 +0800

    2025-06-07 子类对父类值默认设置默认值

---
+++ basic/src/main/java/com/zxw/demo/number/ShortDemo.java
@@ -0,0 +1,21 @@
+package com.zxw.demo.number;
+
+/**
+ * @Author: Ryan
+ * @Date: 2025/6/7 15:58
+ * @Version: 1.0
+ * @Description: about short
+ */
+public class ShortDemo {
+
+    /**
+     * short 类型问题面试题
+     */
+    public void shortMethod1() {
+        short s = 1;
+//        s = s + 1; // 检查异常，因为 1 默认是 int 类型，+1 操作会转成 int 类型，short 接收不了，需要强转
+        s = (short) (s + 1);
+        s += 1; // += 会自动强转
+    }
+
+}
