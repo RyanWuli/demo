e08847612f53f9e253228a9573e0b21b3730ceea
Ryan <9726998998@qq.com>
2025-06-07 18:13:49 +0800

    2025-06-07 强制转换用例备注添加

--- basic/src/main/java/com/zxw/demo/object/ForceConvertDemo.java
+++ basic/src/main/java/com/zxw/demo/object/ForceConvertDemo.java
@@ -6,11 +6,13 @@ import com.zxw.entity.Person;
  * @Author: Ryan
  * @Date: 2022/2/10 15:41
  * @Version: 1.0
- * @Description: add the description
+ * @Description: 强制转换用例
  */
 public class ForceConvertDemo {
 
-
+    /**
+     * object 强转为具体 class 类型之后，参数还是传递的原来的地址对象，所以外层和里层函数都是操作的同一个对象
+     */
     public static Person force(Object force) {
         Person p = null;
         if (force instanceof Person) {
